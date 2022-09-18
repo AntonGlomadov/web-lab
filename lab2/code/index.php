6514dae9fd2f6f069234f5e103cccc144f116502
Anton <AGlomadov@stud.kantiana.ru>
2022-09-18 18:17:32 +0200

    Lab2
    task 1

---
+++ lab2/code/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$very_bad_unclear_name = "15 chicken wings";
+// Write your code here:
+$order = & $very_bad_unclear_name;
+$order = $order . ' , 20 ckiken legs ';
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
