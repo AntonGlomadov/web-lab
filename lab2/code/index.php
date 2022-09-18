253ed2df6aabcb47f6af41c77abc285b109c5358
Anton <AGlomadov@stud.kantiana.ru>
2022-09-18 19:03:42 +0200

    Lab2
    Task 2

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -1,8 +1,24 @@
 <?php
 
+echo nl2br("Task 1:\n");
 $very_bad_unclear_name = "15 chicken wings";
 // Write your code here:
 $order = & $very_bad_unclear_name;
 $order = $order . ' , 20 ckiken legs ';
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+echo nl2br("\n Task 2: \n");
+$var = 1;
+echo $var;
+$var2 = 2;
+echo nl2br("\n". $var2);
+$doubleVar = 1.2;
+echo nl2br("\n" . $doubleVar);
+$var12 = 3 + 9;
+echo nl2br("\n" . $var12);
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo nl2br("\n" . ($last_month-$this_month));
+
+
